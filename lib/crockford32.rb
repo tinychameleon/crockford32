b4e24ebec8ebe09459a7b00e45fe4b77c3b3c9ab
Stephan Tarulli <srt@tinychameleon.com>
2022-02-15 18:33:15 -0800

    Add basic decode without checksum or dash support

--- lib/crockford32.rb
+++ lib/crockford32.rb
@@ -4,5 +4,52 @@ require_relative "crockford32/version"
 
 module Crockford32
   class Error < StandardError; end
-  # Your code goes here...
+
+  DECODE_SYMBOLS = {
+    # Base32 Symbols
+    '0' => 0, 'O' => 0, 'o' => 0,
+    '1' => 1, 'I' => 1, 'i' => 1, 'L' => 1, 'l' => 1,
+    '2' => 2,
+    '3' => 3,
+    '4' => 4,
+    '5' => 5,
+    '6' => 6,
+    '7' => 7,
+    '8' => 8,
+    '9' => 9,
+    'A' => 10, 'a' => 10,
+    'B' => 11, 'b' => 11,
+    'C' => 12, 'c' => 12,
+    'D' => 13, 'd' => 13,
+    'E' => 14, 'e' => 14,
+    'F' => 15, 'f' => 15,
+    'G' => 16, 'g' => 16,
+    'H' => 17, 'h' => 17,
+    'J' => 18, 'j' => 18,
+    'K' => 19, 'k' => 19,
+    'M' => 20, 'm' => 20,
+    'N' => 21, 'n' => 21,
+    'P' => 22, 'p' => 22,
+    'Q' => 23, 'q' => 23,
+    'R' => 24, 'r' => 24,
+    'S' => 25, 's' => 25,
+    'T' => 26, 't' => 26,
+    'V' => 27, 'v' => 27,
+    'W' => 28, 'w' => 28,
+    'X' => 29, 'x' => 29,
+    'Y' => 30, 'y' => 30,
+    'Z' => 31, 'z' => 31,
+    # Base32 Checksum Symbols
+    '*' => 32,
+    '~' => 33,
+    '$' => 34,
+    '=' => 35,
+    'U' => 36, 'u' => 36,
+  }
+
+  def self.decode(str)
+    str.chars.reduce(0) do |result, ch|
+      (result << 5) | DECODE_SYMBOLS[ch]
+    end
+  end
 end
--- test/test_crockford32.rb
+++ test/test_crockford32.rb
@@ -7,7 +7,64 @@ class TestCrockford32 < Minitest::Test
     refute_nil ::Crockford32::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  VALUES = {
+    # Base32 Symbols
+    0 => { decode: ['0', 'O', 'o'], encode: '0' },
+    1 => { decode: ['1', 'I', 'i', 'L', 'l'], encode: '1' },
+    2 => { decode: ['2'], encode: '2' },
+    3 => { decode: ['3'], encode: '3' },
+    4 => { decode: ['4'], encode: '4' },
+    5 => { decode: ['5'], encode: '5' },
+    6 => { decode: ['6'], encode: '6' },
+    7 => { decode: ['7'], encode: '7' },
+    8 => { decode: ['8'], encode: '8' },
+    9 => { decode: ['9'], encode: '9' },
+    10 => { decode: ['A', 'a'], encode: 'A' },
+    11 => { decode: ['B', 'b'], encode: 'B' },
+    12 => { decode: ['C', 'c'], encode: 'C' },
+    13 => { decode: ['D', 'd'], encode: 'D' },
+    14 => { decode: ['E', 'e'], encode: 'E' },
+    15 => { decode: ['F', 'f'], encode: 'F' },
+    16 => { decode: ['G', 'g'], encode: 'G' },
+    17 => { decode: ['H', 'h'], encode: 'H' },
+    18 => { decode: ['J', 'j'], encode: 'J' },
+    19 => { decode: ['K', 'k'], encode: 'K' },
+    20 => { decode: ['M', 'm'], encode: 'M' },
+    21 => { decode: ['N', 'n'], encode: 'N' },
+    22 => { decode: ['P', 'p'], encode: 'P' },
+    23 => { decode: ['Q', 'q'], encode: 'Q' },
+    24 => { decode: ['R', 'r'], encode: 'R' },
+    25 => { decode: ['S', 's'], encode: 'S' },
+    26 => { decode: ['T', 't'], encode: 'T' },
+    27 => { decode: ['V', 'v'], encode: 'V' },
+    28 => { decode: ['W', 'w'], encode: 'W' },
+    29 => { decode: ['X', 'x'], encode: 'X' },
+    30 => { decode: ['Y', 'y'], encode: 'Y' },
+    31 => { decode: ['Z', 'z'], encode: 'Z' },
+    # Base32 Checksum Symbols
+    32 => { decode: ['*'], encode: '*' },
+    33 => { decode: ['~'], encode: '~' },
+    34 => { decode: ['$'], encode: '$' },
+    35 => { decode: ['='], encode: '=' },
+    36 => { decode: ['U', 'u'], encode: 'U' },
+  }.freeze
+
+  REGRESSIONS = [
+    {encoded: "101o1O", decoded: 33587232},
+    {encoded: "IiLl", decoded: 33825},
+  ].freeze
+
+  def test_that_individual_characters_decode_correctly
+    VALUES.each do |val, config|
+      config[:decode].each do |ch|
+        assert_equal val, ::Crockford32.decode(ch), "decode('#{ch}')"
+      end
+    end
+  end
+
+  def test_that_simple_encodings_decode_correctly
+    assert_equal 1234, ::Crockford32.decode("16J"), "decode('16J')"
+    assert_equal 123456789012345, ::Crockford32.decode("3G9230VQVS"), "decode('3G9230VQVS')"
+    assert_equal 1e20, ::Crockford32.decode("2pqhty5nhhoooo"), "decode('2pqhty5nhhoooo')"
   end
 end
